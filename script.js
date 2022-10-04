6ad71eb6cd791d161f9ce9dd99365a77d44e4fbb
unknown <jyotirjy@gmail.com>
2022-10-04 10:33:15 -0400

    completed configuration settings for different possible outcomes

---
+++ script.js
@@ -0,0 +1,12 @@
+function functionality() {
+    let lives = 6
+   const words = ["food", "bool", "hunter", "submit", "valid", "opacity", "prejudice", "threaten", "productive", "forum"]
+const outcomes = {
+    win: "Congratulations You Win",
+    loss: "Game Over! You Died",
+    wrongInput: "Invalid input. Choose a word from A-Z"
+
+}
+// settings for how the game words and different outcomes
+
+} 
