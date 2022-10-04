98f3796f77f9b1e17a02921eeac3f22594721306
unknown <jyotirjy@gmail.com>
2022-10-04 11:24:30 -0400

    updated how random word is generated

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+const currentWord = document.getElementById("random-word")
+
+
+ // settings for how the game words and different outcomes
 function functionality() {
     let lives = 6
-   const words = ["food", "bool", "hunter", "submit", "valid", "opacity", "prejudice", "threaten", "productive", "forum"]
-const outcomes = {
-    win: "Congratulations You Win",
-    loss: "Game Over! You Died",
-    wrongInput: "Invalid input. Choose a word from A-Z"
+    const words = ["food", "rain", "hunter", "submit", "valid", "opacity", "prejudice", "threaten", "productive", "forum"]
+    const outcomes = {
+        win: "Congratulations You Win",
+        loss: "Game Over! You Died",
+        wrongInput: "Invalid input. Choose a word from A-Z"
 
+    }
+   
+
+    // choose random word and append to list in DOM
+    let chosenWord = words[Math.floor(Math.random) * words.length]
+    for (i = 0; i < chosenWord.length; i++) {
+const listItem = document.createElement('li');
+listItem.classList.add('letter') ;
+currentWord.append(listItem)
 }
-// settings for how the game words and different outcomes
-
-} 
+}
