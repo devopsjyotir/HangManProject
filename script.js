4a1492eb25075139f001a1159482bb1eeb550c8f
unknown <jyotirjy@gmail.com>
2022-10-04 23:35:06 -0400

    Changed landing html content and style for the game board

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-const currentWord = document.getElementById("random-word")
+let currentWord = document.getElementById("random-word")
+const words = ["food", "rain", "hunter", "submit", "valid", "opacity", "prejudice", "threaten", "productive", "forum"]
+let lives = 6
+const outcomes = {
+    win: "Congratulations You Win",
+    loss: "Game Over! You Died",
+    wrongInput: "Invalid input. Choose a word from A-Z"
 
-
- // settings for how the game words and different outcomes
-function functionality() {
-    let lives = 6
-    const words = ["food", "rain", "hunter", "submit", "valid", "opacity", "prejudice", "threaten", "productive", "forum"]
-    const outcomes = {
-        win: "Congratulations You Win",
-        loss: "Game Over! You Died",
-        wrongInput: "Invalid input. Choose a word from A-Z"
-
-    }
-   
-
-    // choose random word and append to list in DOM
-    let chosenWord = words[Math.floor(Math.random) * words.length]
-    for (i = 0; i < chosenWord.length; i++) {
-const listItem = document.createElement('li');
-listItem.classList.add('letter') ;
-currentWord.append(listItem)
 }
-}
+// settings for how the game words and different outcomes
+// function functionality() {
+//     // choose random word and append to list in DOM
+//     let chosenWord = words[Math.floor(Math.random) * words.length]
+//     let chosenWordLength = chosenWord.length
+//     for (i = 0; i < chosenWordLength; i++) {
+//         const listItem = document.createElement('li');
+//         listItem.classList.add('letter');
+//         currentWord.append(listItem)
+//     }
+// }
+
+// function check(ev) {
+//     // const letterElements = document.querySelectorAll('.word .letter');
+//    let character = ev.key;
+//  listItem.innerText = `${character}`
+// }
+
+
+// window.addEventListener('keyup', check)
