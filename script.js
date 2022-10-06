00027f764d7395b2282dea54559fc2fbe2f1fec4
unknown <jyotirjy@gmail.com>
2022-10-06 11:47:57 -0400

    created restart condition and added event listener for when the restart button is clicked

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+ () => {
+
+
 let currentWord = document.getElementById("random-word")
 
 // settings for how the game words and different outcomes
@@ -5,8 +9,8 @@ function functionality() {
     const words = ["food", "rain", "hunter", "submit", "valid", "opacity", "prejudice", "threaten", "productive", "forum"]
     let lives = 6
     const outcomes = {
-        win: "Congratulations You Win",
-        loss: "Game Over! You Died",
+        winner: "Congratulations You Win",
+        loser: "Game Over! You Died",
         wrongInput: "Invalid input. Choose a word from A-Z"
 
     }
@@ -17,7 +21,9 @@ const restartBtn = document.getElementById('restart-btn')
 const remainingGuess = document.getElementById('remaining-guess')
 const output = document.getElementById('output')
 
-let letterGuessed = input
+const letters = document.getElementById("letters");
+letters.innerHTML = '<li class="current-word">Current word:</li>';
+
 
 // choose random word and append to list in DOM
 let chosenWord = words[Math.floor(Math.random) * words.length]
@@ -28,4 +34,15 @@ let chosenWordLength = chosenWord.length
 // }
 //     }
 
+function gameOver(winner){
+    if(win){
+        output.innerText = outcomes.winner
+    }else {output.innerText = outcomes.loser}
+}
+guess.value = ""
+ }
 
+// reloads the game functionality when the restart button is clicked.
+restartBtn.addEventListener('click', functionality)
+
+ window.onload = functionality()
