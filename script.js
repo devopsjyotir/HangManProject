f78c512afdbac81f98d438c23acbe4a27c9e6b2e
unknown <jyotirjy@gmail.com>
2022-10-06 19:29:28 -0400

    succesfuly wrote code to display blank spaces and updated global variables

--- script.js
+++ script.js
@@ -1,48 +1,62 @@
 
- () => {
-
-
-let currentWord = document.getElementById("random-word")
-
-// settings for how the game words and different outcomes
-function functionality() {
-    const words = ["food", "rain", "hunter", "submit", "valid", "opacity", "prejudice", "threaten", "productive", "forum"]
-    let lives = 6
-    const outcomes = {
-        winner: "Congratulations You Win",
-        loser: "Game Over! You Died",
-        wrongInput: "Invalid input. Choose a word from A-Z"
-
-    }
-}
+let string
 
 const guess = document.querySelector('.guess')
 const restartBtn = document.getElementById('restart-btn')
 const remainingGuess = document.getElementById('remaining-guess')
 const output = document.getElementById('output')
 
-const letters = document.getElementById("letters");
-letters.innerHTML = '<li class="current-word">Current word:</li>';
+const answerLetters = document.getElementById("letters");
+
+const answerArr = []
+// settings for how the game words and different outcomes
+
+// defining the array of words to be used
+const words = ["food", "rain", "hunter", "submit", "valid", "opacity", "prejudice", "threaten", "productive", "forum"]
+let lives = 6
 
 
-// choose random word and append to list in DOM
-let chosenWord = words[Math.floor(Math.random) * words.length]
-let chosenWordLength = chosenWord.length
-//     for (i = 0; i < chosenWordLength; i++) {
-// if(guess === chosenWord[i]){
+// choose random word 
+const chosenWord = words[Math.floor(Math.random) * words.length]
+const chosenWordLength = chosenWord.length
 
-// }
-//     }
-
-function gameOver(winner){
-    if(win){
-        output.innerText = outcomes.winner
-    }else {output.innerText = outcomes.loser}
+// storing possible outcomes in an object
+const outcomes = {
+    winner: "Congratulations You Win",
+    loser: "Game Over! You Died",
+    wrongInput: "Invalid input. Choose a word from A-Z"
 }
-guess.value = ""
- }
+
+
+function setup() {
+    // setting up blank spaces to be displayed before a guess is made
+    for (i = 0; i < chosenWordLength; i++) {
+        answerArr[i] = "_"
+    }
+
+    string = answerArr.join("")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// function gameOver(winner){
+//     if(win){
+//         output.innerText = outcomes.winner
+//     }else {output.innerText = outcomes.loser}
+// }
+// guess.value = ""
+
 
 // reloads the game functionality when the restart button is clicked.
 restartBtn.addEventListener('click', functionality)
 
- window.onload = functionality()
+window.onload = functionality()
