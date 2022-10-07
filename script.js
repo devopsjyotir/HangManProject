47b64b4e7154f82ee240642066d9e24a33217e7d
unknown <jyotirjy@gmail.com>
2022-10-07 02:06:44 -0400

    written down winning condition and stored correct answer in array

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 // // defining the array of words to be used
-const words = ["food", "rain", "hunter", "submit", "valid", "opacity", "prejudice", "threaten", "productive", "forum"];
-
+const win = document.createElement('p');
+const h1 = document.querySelector(".landingh1")
+const words = ["food", "hunter", "opacity", "prejudice", "threaten", "productive", "forum"];
+const body = document.querySelector('body')
 // storing possible outcomes in an object
 const outcomes = {
-    winner: "Congratulations You Win",
+    winner: "Congratulations You Win!",
     loser: "Game Over! You Died",
     wrongInput: "Invalid input. Choose a word from A-Z"
 }
@@ -12,37 +14,70 @@ const outcomes = {
 
 // // choose random word 
 const chosenWord = words[Math.floor(Math.random() * words.length)];
-
+let storeAnswer = []
 let updateBlank;
 let guesses = 10;
-const selectedWordArr = [];
+const correctAnswer = [];
+const isGameOver = () => {
+    if (storeAnswer.length === correctAnswer.length) {
+        return true
+    }
+}
+
+const isALoss = () => {
+    if (guesses < 1) {
+        return true
+    }
+}
 
 function startGame() {
     for (let i = 0; i < chosenWord.length; i++) {
-        selectedWordArr[i] = "_";
+        correctAnswer[i] = "_";
     }
 
-    updateBlank = selectedWordArr.join(" ");
+    updateBlank = correctAnswer.join(" ");
     document.getElementById("answer").innerHTML = updateBlank;
 }
 
 
 // setting up blank spaces to be displayed before a guess is made
 function filling() {
+    // to select what letter has been typed
     let letter = document.getElementById("letter").value;
     if (letter.length > 0) {
         for (let i = 0; i < chosenWord.length; i++) {
             if (chosenWord[i] === letter) {
-                selectedWordArr[i] = letter;
+                correctAnswer[i] = letter;
+                storeAnswer.push(letter);
+            
+                console.log(correctAnswer.toString())
+
             }
 
         }
+        if (isGameOver()) {
+            h1.innerText = outcomes.winner
+        }
+
+        // if(isALoss()){
+        //     h1.innerText = outcomes.loser
+        // }
 
         guesses--;
         document.getElementById("remaining-guess").innerHTML = "remaining guesses: " + guesses;
-        document.getElementById("answer").innerHTML = selectedWordArr.join(" ");
+        document.getElementById("answer").innerHTML = correctAnswer.join(" ");
+
+
     }
+
+
+
 }
+
+
+
+
+
 
 
 // EventListeners
